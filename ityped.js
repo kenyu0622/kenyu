174c4c77f29832c4ab1a0b91450fb28ba39bb1b1
kenyu0622 <61593042+kenyu0622@users.noreply.github.com>
2020-02-29 00:45:33 +0900

    Create ityped.js

---
+++ ityped.js
@@ -0,0 +1,10 @@
+ityped.init('#ityped', {
+  strings: ['HTML','CSS','JQuery','PHP','Wordpress'],
+  typeSpeed: 160,
+  backSpeed: 40,
+  startDelay: 500,
+  backDelay: 1500,
+  loop: true,
+  showCursor: true,
+  curdorChar: "|"
+});
